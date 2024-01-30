b8be2749e0a8ec33dbb8a8f0ed9b0f7e858074a6
RosanneVN <annevazquez05@gmail.com>
2024-01-30 16:43:47 -0500

    start project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main>
+      <div className="board">
+        <section className="screen"></section>
+        <section className="keyboard">contenedores</section>
+      </div>
+    </main>
+  );
+}
+export default App;
