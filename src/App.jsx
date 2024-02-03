02640e6993d856f47d0bdb8137d1dedb0923dc42
RosanneVN <annevazquez05@gmail.com>
2024-02-02 21:20:53 -0500

    letter keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import { useState } from "react";
 import "./App.css";
 
+const letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "n",
+  "m",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "CapsLk",
+];
+
 function App() {
   const [color, setColor] = useState("black");
-
   const handleClick = (newColorBoard) => {
-      setColor(newColorBoard)
+    setColor(newColorBoard);
   };
 
   return (
@@ -13,25 +40,33 @@ function App() {
       <div className={`board ${color}`}>
         <div className="content">
           <section className="screen"></section>
-          <section className="keyboard">contenedores</section>
+          <div className="keyboard-content">
+            <section className="keyboard">
+              {letters.map((letter) => (
+                <div className="styleLetter" key={letter}>
+                  {letter}
+                </div>
+              ))}
+            </section>
+          </div>
         </div>
         <div className="button">
           <div className="button-content">
             <button
               className="color-button rose-button"
-              onClick={()=>handleClick("rose")}
+              onClick={() => handleClick("rose")}
             ></button>
             <button
               className="color-button grey-button"
-              onClick={()=>handleClick("grey")}
+              onClick={() => handleClick("grey")}
             ></button>
             <button
               className="color-button blue-button"
-              onClick={()=>handleClick("blue")}
+              onClick={() => handleClick("blue")}
             ></button>
             <button
               className="color-button dark-button"
-              onClick={()=>handleClick("dark")}
+              onClick={() => handleClick("dark")}
             ></button>
           </div>
         </div>
