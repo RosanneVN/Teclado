ecd1f8b2e2f92a00f86ba5b9a1c9bd6750bf1898
RosanneVN <annevazquez05@gmail.com>
2024-02-22 00:04:20 -0500

    fix  letter keys

--- src/components/Letters.jsx
+++ src/components/Letters.jsx
@@ -15,36 +15,119 @@ export const Letters = () => {
     setWriten(writen.substring(0, writen.length - 1));
   };
 
-  const handleClick = () => {
+  const CapsLockClick = () => {
     setKeyM(!keyM);
   };
 
   return (
     <section className="keyboard-base">
       {KEYS.map((letter) => {
-        if (letter.name == "CapsLk") {
+        if (letter.name == "CapsLock") {
           return (
-            <button className="key" key={letter.name} onClick={handleClick}>
+            <button className="key capslock" key={letter.name} onClick={CapsLockClick}>
               {letter.name}
             </button>
           );
         } else if (letter.name == "Delete") {
           return (
-            <button className="key" key={letter.name} onClick={deleteClick}>
+            <button className="key delete" key={letter.name} onClick={deleteClick}>
               {letter.name}
             </button>
           );
         } else if (letter.name == "Space") {
           return (
             <button
-              className="key"
+              className="key space"
               key={letter.name}
               onClick={() => writeClick(" ")}
             >
               {letter.name}
             </button>
           );
-        }  else if (letter.name == "Tab") {
+        } else if (letter.name == "Tab") {
+          return (
+            <button
+              className="key tab"
+              key={letter.name}
+              onClick={() => writeClick("    ")}
+            >
+              {letter.name}
+            </button>
+          );
+        } else if (letter.name == "Enter") {
+          return (
+            <button
+              className="key enter"
+              key={letter.name}
+              onClick={() => writeClick("    ")}
+            >
+              {letter.name}
+            </button>
+          );
+        } else if (letter.name == "Shiftl") {
+          return (
+            <button
+              className="key leftshift"
+              key={letter.name}
+              onClick={() => writeClick("    ")}
+            >
+              {letter.name}
+            </button>
+          );
+        } else if (letter.name == "Alt") {
+          return (
+            <button
+              className="key"
+              key={letter.name}
+              onClick={() => writeClick("    ")}
+            >
+              {letter.name}
+            </button>
+          );
+        } else if (letter.name == "Fn") {
+          return (
+            <button
+              className="key fn"
+              key={letter.name}
+              onClick={() => writeClick("    ")}
+            >
+              {letter.name}
+            </button>
+          );
+        } else if (letter.name == "Command") {
+          return (
+            <button
+              className="key command"
+              key={letter.name}
+              onClick={() => writeClick("    ")}
+            >
+              {letter.name}
+            </button>
+          );
+        }
+        else if (letter.name == "/") {
+          return (
+            <button
+              className="key backslash"
+              key={letter.name}
+              onClick={() => writeClick("    ")}
+            >
+              {letter.name}
+            </button>
+          );
+        }
+        else if (letter.name == "Shiftr") {
+          return (
+            <button
+              className="key rightshift"
+              key={letter.name}
+              onClick={() => writeClick("    ")}
+            >
+              {letter.name}
+            </button>
+          );
+        }
+        else if (letter.name == "Ctrl") {
           return (
             <button
               className="key"
@@ -55,15 +138,26 @@ export const Letters = () => {
             </button>
           );
         }
-         {
-          letter.name = keyM ? letter.name.toUpperCase() : letter.name;
+        else if (letter.name == "Ctrll") {
+          return (
+            <button
+              className="key leftctrl"
+              key={letter.name}
+              onClick={() => writeClick("    ")}
+            >
+              {letter.name}
+            </button>
+          );
+        }
+        {
+          const name = keyM ? letter.name.toUpperCase() : letter.name;
           return (
             <button
               className="key"
               key={letter.name}
-              onClick={() => writeClick(letter.name)}
+              onClick={() => writeClick(name)}
             >
-              {letter.name}
+              {name}
             </button>
           );
         }
